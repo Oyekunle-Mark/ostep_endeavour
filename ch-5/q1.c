789e947fe99a857a0248247e7b1fcd29a4817401
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-13 18:52:14 +0100

    Logged all in new lines

--- ch-5/q1.c
+++ ch-5/q1.c
@@ -7,9 +7,9 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    printf("The parent process id is %d", (int) getpid());
+    printf("The parent process id is %d\n", (int) getpid());
     int x = 100;
-    printf("Before fork, x is %d", x);
+    printf("Before fork, x is %d\n", x);
 
     int cid = fork();
 
@@ -17,13 +17,14 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Error creating fork");
         exit(1);
     } else if (cid == 0) {
-        printf("In child process (%d), x is %d", (int) getpid(), x);
+        printf("In child process (%d), x is %d\n", (int) getpid(), x);
         x = -23;
-        printf("In child process, new value of x is %d", x);
+        printf("In child process, new value of x is %d\n", x);
     } else {
-        printf("In parent process, x is %d", x);
+        wait(NULL);
+        printf("In parent process, x is %d\n", x);
         x = 5330;
-        printf("In parent process, new value of x is %d", x);
+        printf("In parent process, new value of x is %d\n", x);
     }
 
     return EXIT_SUCCESS;
