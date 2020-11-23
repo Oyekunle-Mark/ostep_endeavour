5a415156a8d9483819697d913aa44bfa5957eb3f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-23 16:13:41 +0100

    Allocates and prints entries

--- ch-13/memory_user.c
+++ ch-13/memory_user.c
@@ -1,7 +1,21 @@
 //
 // Created by Oyekunle Oloyede on 23/11/2020.
 //
+#include <stdio.h>
+#include <stdlib.h>
+
+#define KILOBYTE 1024
 
 int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        fprintf(stderr, "No command line arguments provided");
+        exit(1);
+    }
 
+    char *ptr = malloc((int) argv(1) * KILOBYTE * KILOBYTE);
+
+    for (int i; i < sizeof(ptr); ++i)
+        printf("%d - %c", i, ptr[i]);
+
+    return 0;
 }
