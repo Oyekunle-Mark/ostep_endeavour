5c601ccf530cc472d957f6d152092b35c06ad8d8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-23 18:16:13 +0100

    the null program

--- ch-14/null.c
+++ ch-14/null.c
@@ -1,7 +1,11 @@
 //
 // Created by Oyekunle Oloyede on 23/11/2020.
 //
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    int *ptr = (int *) malloc(sizeof(int));
+    ptr = NULL;
 
+    free(ptr);
 }
